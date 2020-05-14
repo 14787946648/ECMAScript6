234cdf9ead3cc07dc8da859ae4c18b3f76d17988
aurora <wh14787946648@outlokk.com>
2020-05-14 15:07:49 +0800

    Game

--- web/js/random.js
+++ web/js/random.js
@@ -2,13 +2,14 @@
 
 //获取随机数
 let random_Num=Math.floor(Math.random()*100);
+let count=0;
 
-//再来一次
+//再来一次刷新网页
 function again(){
     window.location.reload();
 }
 
-//取消
+//取消推出网页
 function Cancel() {
     window.opener = null;
     window.open('', '_self');
@@ -17,23 +18,27 @@ function Cancel() {
 
 function checkNum(){
     //获取文本框内容
-    let Text_num=Number.parseFloat(document.querySelector("#num").value.trim());
+    let text_Num=Number.parseFloat(document.querySelector("#num").value.trim());
 
     //判断逻辑
-    if (Number.isInteger(Text_num)){
-        if ( Text_num>=0 && Text_num < 100){
-            if (Text_num==random_Num){
-                let style_Display=document.querySelector("#container");
-                style_Display.getAttributeNode("style").value="display:none";
-                let style_Display_Img=document.querySelector("#container02");
-                style_Display_Img.getAttributeNode("style").value="display:block";
-            }else if(Text_num>random_Num){
+    if (Number.isInteger(text_Num)){
+        if ( text_Num>=0 && text_Num < 100){
+            if (text_Num==random_Num){
+                ++count;
+                // document.querySelector("#container").getAttributeNode("style").value="display:none";
+                document.querySelector("#container").style.display="none";
+                // document.querySelector("#container02").getAttributeNode("style").value="display:block";
+                document.querySelector("#container02").style.display="block";
+                document.querySelector("#container02_Txt").innerHTML=`"恭喜你！你用了${count}次之后......答对了！再来一次吗？"`;
+            }else if(text_Num>random_Num){
                 alert("你猜的数字太大了！")
-            }else if(Text_num<random_Num){
+                ++count;
+            }else if(text_Num<random_Num){
                 alert("你猜的数字太小了！")
+                ++count;
             }
         }else {
-            alert("请输入0-99的整数！");
+            alert("请确认输入数据范围：0-99！");
         }
     }else{
         alert("您是输入的数据不是整数！")
